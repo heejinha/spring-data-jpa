475313cc418896ecdbc87369d85000b5288194f7
hjha <hjha@gaia3d.com>
2023-07-17 22:00:43 +0900

    feat: 리포지토리에 @Query 메소드 정의 코드 추가
    - 어플리케이션 로딩 시점에 쿼리 파싱하기 때문에 오류 확인 가능한 장점으로 실무에서 많이 사용

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -17,7 +17,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
      * 쿼리 어노테이션 선언하지 않아도 동일하게 적용 됨
      *  1순위가 Member객체에 선언된 findByUsername 네임드 쿼리를 서치 함. 
      *  없는 경우 2순위로 쿼리 메소드 생성
-     * 하지만 현업에서는 거의 사용하지 않음
+     * 
+     * 하지만 실무에서는 거의 사용하지 않음
+     *  => 리포지토리 메소드에 @Query 정의하는 방식을 많이 씀
      * 
      * [네임드쿼리의 장점]
      *   어플리케이션 로딩 시점에 네임드 쿼리 파싱을 먼저 실행 함.
@@ -25,4 +27,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
      */ 
     @Query(name = "Member.findByUsername")  
     List<Member> findByUsername(@Param("username")  String username);
+
+    /**
+     * 실무에서는 리포지토리에 @Query 메소드 정의하는 방식을 많이 사용 함.
+     *   어플리케이션 로딩 시점에 쿼리 파싱하기 때문에 오류 확인 가능
+     */
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
