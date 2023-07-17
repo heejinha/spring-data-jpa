b8133b53868ece836508ed60cf2e14f6b8824282
hjha <hjha@gaia3d.com>
2023-07-17 22:11:32 +0900

    feat: DTO를 이용한 @Query 생성 코드 추가

---
+++ src/main/java/study/datajpa/dto/MemberDTO.java
@@ -0,0 +1,17 @@
+package study.datajpa.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+    private Long id;
+    private String username;
+    private String teamName;
+}
